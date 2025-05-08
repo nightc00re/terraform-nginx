117035e6db589e0da7b23028fdb7793ccde6609d
nightc00re <michelyoseph2@gmail.com>
2025-05-08 17:45:13 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,22 @@
 variable "aws_region" {
   description = "The AWS region to deploy to"
   type        = string
-  default     = "us-east-1"
+  default     = "us-east-2" # Changed to us-east-2
 }
 
 variable "key_name" {
   description = "The name of the EC2 key pair to use"
   type        = string
-  default     = "ronaldo.pem"  # Make sure this matches your key pair name
+  default     = "lebronjames" # Changed to lebronjames
 }
 
 variable "instance_type" {
-  description = "The type of EC2 instance to launch"
+  description = "EC2 instance type"
   type        = string
-  default     = "t3.micro"
+  default     = "t2.micro"
+}
+variable "project" {
+  description = "Project name tag"
+  type        = string
+  default     = "nginx-vpc"
 }
